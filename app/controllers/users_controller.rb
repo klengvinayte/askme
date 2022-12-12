9937b88923dad447c27998b4462a03375321e277
kristina_lengvinayte <klengvinayte@gmail.com>
2022-12-12 23:40:00 +0200

    Authenticate cookies session

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def new
+    session[:current_time] = Time.now
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.create(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+    end
+
+    redirect_to root_path, notice: 'Вы успешно зарегистрировались'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resource :session, only: %i[new create destroy]
+  resources :users, only: %i[new create]
+
   root to: 'questions#index'
 
   resources :questions do
