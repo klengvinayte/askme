1206ae794884d5c3b2068de4fc336c2d54d432e9
kristina_lengvinayte <klengvinayte@gmail.com>
2023-01-09 21:47:07 +0200

    added redirect

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,12 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.find_by!(name: params[:name])
-    @questions = @tag.questions.order(created_at: :desc)
+
+    if
+      @tag.questions.empty?
+      redirect_to root_path, notice: "Нет вопросов с хэштэгом ##{@tag.name}"
+    else
+      @questions = @tag.questions.order(created_at: :desc)
+    end
   end
 end
